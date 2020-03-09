95a4776502bb0ece4993e677076fd0f423214015
nickariley <nickariley@gmail.com>
2020-03-09 11:33:13 -0500

    Added TicTacToe, clock, date, animation practice, changecontentpractice

---
+++ portfolio-responsive/js/currentdate.js
@@ -0,0 +1,60 @@
+
+
+    //date      month         Year
+    
+    
+    var today =new Date();
+
+
+    var dd = today.getDate();
+    
+    var yyyy = today.getFullYear();
+    
+    var mm = today.getMonth();
+    
+    var month = ["jan", "feb", "march", "april", "may", "june", "july", "aug", "sep", "oct", "nov", "dec"];
+    
+    today=dd+' '+month[mm]+' '+yyyy;
+    document.write(today);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var today = new Date();
+    var dd = today. getDate();
+    var mm = today.getMonth();
+    var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Des" ];
+    var yyyy = today.getFullYear();
+    
+    today = dd+' '+month[mm]+' '+yyyy;
+    console.log(today);
+    
+    
